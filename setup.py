b15b68ba9be6e618d44d0172457ffed45e7ac83d
michael34435 <michael34435@gmail.com>
2015-11-25 17:44:14 +0800

    minor fix, add distutils support

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='python-upload',
     url='https://github.com/CapsLock-Studio/python-upload',
     zip_safe=True,
     version='0.2a',
-    packages=find_packages(),
-    include_package_data=True,
     entry_points={
         'console_scripts': [
             'upload=upload:main'
